61954fb574a7e94c863037a723e4b315eb52c513
JhonnyJohnJones <jhonnyjohnjones1.0@gmail.com>
2026-06-08 15:54:28 -0300

    feat(health): Add health check to redis

--- src/health/health.controller.ts
+++ src/health/health.controller.ts
@@ -3,6 +3,7 @@ import { Controller, Get } from '@nestjs/common';
 import { HealthCheck, HealthCheckService } from '@nestjs/terminus';
 
 import { PrismaService } from '../common/prisma/prisma.service';
+import { RedisService } from '../common/redis/redis.service';
 
 @Controller('health')
 export class HealthController {
@@ -10,6 +11,7 @@ export class HealthController {
     private readonly health: HealthCheckService,
 
     private readonly prisma: PrismaService,
+    private readonly redis: RedisService,
   ) {}
 
   @Get()
@@ -26,10 +28,26 @@ export class HealthController {
     return this.health.check([
       async () => {
         // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        await this.prisma.$queryRaw`SELECT 1`;
+        await this.prisma.$executeRaw`
+          SELECT 1
+        `;
 
         return {
           database: {
+            status: 'up',
+          },
+        };
+      },
+
+      async () => {
+        const pong = await this.redis.ping();
+
+        if (pong !== 'PONG') {
+          throw new Error('Redis unavailable');
+        }
+
+        return {
+          redis: {
             status: 'up',
           },
         };
