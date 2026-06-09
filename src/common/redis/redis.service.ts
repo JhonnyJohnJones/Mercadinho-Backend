54760cfe2b7bbae5088b31683a2f5b91b722d1db
JhonnyJohnJones <jhonnyjohnjones1.0@gmail.com>
2026-06-09 01:36:51 -0300

    feat(redis): Add more redis configuration

--- src/common/redis/redis.service.ts
+++ src/common/redis/redis.service.ts
@@ -8,6 +8,14 @@ export class RedisService extends Redis {
     super({
       host: process.env.REDIS_HOST,
       port: Number(process.env.REDIS_PORT),
+
+      retryStrategy(times) {
+        return Math.min(times * 50, 2000);
+      },
     });
+  }
+
+  async onModuleDestroy() {
+    await this.quit();
   }
 }
